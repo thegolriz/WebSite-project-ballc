b390bde38dd673bccf9fd4d99e39876413008637
Anis <agolriz@unca.edu>
2025-03-23 00:47:22 -0400

    Added Alembic migrations and updated dependencies

--- bailee-ann-llc-backend/app.py
+++ bailee-ann-llc-backend/app.py
@@ -1,8 +1,11 @@
-from website import create_app
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from website import create_app, db 
 
 
 app = create_app()
+migrate = Migrate(app, db) 
 
 if __name__ == "__main__":
     app.run(debug=True)
