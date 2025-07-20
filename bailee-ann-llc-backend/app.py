6a08b1e52da2c106d73fc30c97ef1274285377bc
Anis Golriz <anisemmagolriz@gmail.com>
2025-07-19 23:31:56 -0400

    modified docker file, added poetry, did some cleanup

--- bailee-ann-llc-backend/app.py
+++ bailee-ann-llc-backend/app.py
@@ -1,12 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from website import create_app, db 
 
+from website import create_app, db
 
 app = create_app()
-migrate = Migrate(app, db) 
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
