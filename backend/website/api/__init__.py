39cde0bfff1c5550b367e0db6214743438b081e7
Anis Golriz <anisemmagolriz@gmail.com>
2025-08-04 04:12:34 -0400

    Created mock db for test suit and more

---
+++ backend/website/api/__init__.py
@@ -0,0 +1,18 @@
+# This file holds the api's for the website
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    db.init_app(app)
+
+    return app
